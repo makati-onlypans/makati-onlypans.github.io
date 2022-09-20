de2b0208fd1d8f3c255c7a45aa89144e46a02579
makati-onlypans <makati.onlypans@gmail.com>
2022-09-20 17:56:42 +0400

    Update ViewUser.jsx

--- src/components/ViewUser.jsx
+++ src/components/ViewUser.jsx
@@ -1,60 +1,55 @@
 import React from 'react';
 import { getAllUsers } from '../service/api';
-import {useParams } from 'react-router-dom';
-import { useState,useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import Navbar from './NavBar';
-const userInitialState ={
+const userInitialState = {
     name: "",
-    email: "",
-    title: "",
-    mobile: "",
+    description: "",
+    price: "",
     photo: "",
-    companyName: "",
-    department: ""
+    departmentName: ""
 
 }
 const Viewuser = () => {
     const [user, setUser] = useState(userInitialState);
-    const {name,email,title,mobile,photo,companyName, department} = user;
-    const {id} = useParams();
-    const loadUserData = async () =>{
+    const { name, description, price, photo, departmentName } = user;
+    const { id } = useParams();
+    const loadUserData = async () => {
         const response = await getAllUsers(id);
         setUser(response.data);
     }
-    useEffect (()=>{
+    useEffect(() => {
         loadUserData();
     }, []);
 
     return (
         <div className="container mt-3">
-        <Navbar/>
-        <h1>View</h1>
-        <div className="container">
-        <div className="row">
-                <div className="col-md-6">
-                <div className="card my-2 list-group-item-success shadow-lg">
-                    <div className="card-body">
-                        <div className="row align-items-center">
-                            <div className="col-sm-4">
-                                <img style={{ width: 100, height:100}} src={photo} alt="logo"/>
-                            </div>
-                            <div className="col-sm-7">
-                                <ul className="list-group">
-                                    <li className="list-group-item">Name: <span className="fw-bold">{name}</span></li>
-                                    <li className="list-group-item">Email: <span className="fw-bold">{email}</span></li>
-                                    <li className="list-group-item">Mobile: <span className="fw-bold">{mobile}</span></li>
-                                    <li className="list-group-item">Position: <span className="fw-bold">{title}</span></li>
-                                    <li className="list-group-item">Company: <span className="fw-bold">{companyName}</span></li>
-                                    <li className="list-group-item">Department: <span className="fw-bold">{department}</span></li>   
-                                </ul>
+            <Navbar />
+            <h1>View</h1>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-10">
+                        <div className="card my-2">
+                            <div className="card-body">
+                                <div className="row align-items-center">
+                                    <div className="col-md-6">
+                                        <img className="img-fluid" src={photo} alt="logo" />
+                                        {/* <img style={{ width: 300, height: 300 }} src={photo} alt="logo" /> */}
+                                    </div>
+                                    <div className="col-md-6 align-text-top">
+                                        <h5>Name:{name}</h5>
+                                        <h5>Description:{description}</h5>
+                                        <h5>Price:P{price}</h5>
+                                        <h5>Category:{departmentName}</h5>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div> 
+                    </div>
                 </div>
             </div>
-    </div>
-    </div>
-    </div>
+        </div>
     );
 }
 
