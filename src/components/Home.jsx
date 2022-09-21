42fcb3230893dfad5ff1b2ba5b7b1a66ba3f192d
SselJay <russeljaybuan@gmail.com>
2022-09-21 10:47:28 +0800

    UserHome

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import UserNavbar from '../usercomponents/UserNavBar'
 import UserFooter from '../usercomponents/UserFooter';
+import UserHome from '../usercomponents/UserHome';
 const Home = () => {
     return (
         <div className='container'>
             <UserNavbar/>
+            <UserHome/>
             <UserFooter/>
         </div>
     );
