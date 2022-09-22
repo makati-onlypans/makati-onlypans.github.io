184b021b5f5b274b4ac56d7fae0c795f68cafdb3
SselJay <russeljaybuan@gmail.com>
2022-09-22 10:21:59 +0800

    Userview added

--- src/ComponentRoute.js
+++ src/ComponentRoute.js
@@ -5,6 +5,7 @@ import Edituser from './components/EditUser';
 import Allusers from './components/AllUsers';
 import Notfound from './components/NotFound';
 import Viewuser from './components/ViewUser';
+import UserView from './usercomponents/UserView';
 import UserAboutUs from './usercomponents/UserAboutUs';
 import UserEvents from './usercomponents/UserEvents';
 import { Route, Routes} from 'react-router-dom';
@@ -19,7 +20,8 @@ const Componentroute = () => {
             <Route path='/useraboutus' element={<UserAboutUs/>}/>
             <Route path='/userevents' element={<UserEvents/>}/>
             <Route path='/usermenu' element={<UserMenu/>}/>
-            <Route path='usermenu/view/:id' element={<Viewuser/>}/>
+            <Route path='/view/:id' element={<Viewuser/>}/>
+            <Route path='usermenu/view/:id' element={<UserView/>}/>
             <Route path='/edit/:id' element={<Edituser/>}/>
             <Route path='*' element={<Notfound/>}/>
                               
---
+++ src/usercomponents/UserView.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UserNavBar from "./UserNavBar";
+import UserFooter from "./UserFooter";
+
+export default function UserView (){
+    return(
+        <>
+            <UserNavBar/>
+            <h1>test</h1>
+            <UserFooter/>
+        </>
+    )
+}
