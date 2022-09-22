5f7bb96cf2ed52394963052865dbf6f12c96065b
makati-onlypans <makati.onlypans@gmail.com>
2022-09-22 13:01:29 +0400

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
-    <nav className="navbar navbar-light bg-light container">
-        <div className="container-fluid">
-            <Link to="/" className="navbar-brand h1">
-                <i className="fa fa-home"></i>Home
-            </Link>
-            <Link to="/all" className='navbar-toggler text-decoration-none'>
-                <i className="fa fa-user"></i>View All
-            </Link>
-            <Link to="/add" className='navbar-toggler text-decoration-none'>
-                <i className="fa fa-plus"></i>Add User
-            </Link>
-        </div>
-    </nav>
+        <nav className="navbar navbar-light bg-light container">
+            <div className="container-fluid">
+                <Link to="/" className="navbar-brand h1">
+                    <i className="fa fa-home"></i>Home
+                </Link>
+                <Link to="/all" className='navbar-toggler text-decoration-none'>
+                    <i className="fa fa-user"></i>View All
+                </Link>
+                <Link to="/add" className='navbar-toggler text-decoration-none'>
+                    <i className="fa fa-plus"></i>Add Menu Item
+                </Link>
+            </div>
+        </nav>
     );
 }
 
