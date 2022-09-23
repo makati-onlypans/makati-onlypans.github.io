1e048d0725adf8b06131d79d6720ad065d1e2497
SselJay <russeljaybuan@gmail.com>
2022-09-23 14:07:53 +0800

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div className="container-fluid m-0 p-0">
-        <nav className="navbar navbar-light bg-light container-fluid">
+        <nav className="navbar navbar-light bg-danger container-fluid">
             <div className="container-fluid">
-                <Link to="/" className="navbar-brand h1">
-                    <i className="fa fa-home"></i>Home
+                <Link to="/" className="navbar-brand h1 text-white">
+                    <i className="fa fa-home text-white"></i>Home
                 </Link>
-                <Link to="/admin" className='navbar-toggler text-decoration-none'>
-                    <i className="fa fa-user"></i>View All
+                <Link to="/admin" className='navbar-toggler text-decoration-none text-white'>
+                    <i className="fa fa-user text-white"></i>View Menu List
                 </Link>
-                <Link to="/add" className='navbar-toggler text-decoration-none'>
-                    <i className="fa fa-plus"></i>Add Menu Item
+                <Link to="/add" className='navbar-toggler text-decoration-none text-white'>
+                    <i className="fa fa-plus text-white"></i>Add Menu Item
                 </Link>
             </div>
         </nav>
